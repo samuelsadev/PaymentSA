96b18938bafab8feaaa830866b3d4893723e85f8
samuelsadev <samuelsa.dev@gmail.com>
2025-02-13 12:46:33 -0300

    first commit

---
+++ src/main/java/com/projectsa/ProducerSA/ProducerSaApplication.java
@@ -0,0 +1,13 @@
+package com.projectsa.ProducerSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerSaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectsa/ProducerSA/ProducerSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsa.ProducerSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
